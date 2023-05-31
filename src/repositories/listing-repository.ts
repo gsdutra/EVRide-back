9959e259dac717a5400d475006cc92f3900722d5
gabrielsd <gabrielsprecherdutra@gmail.com>
2023-05-31 19:15:54 -0300

    feat: create listing search route

--- src/repositories/listing-repository.ts
+++ src/repositories/listing-repository.ts
@@ -1,6 +1,6 @@
 import prisma from '@/config/database';
 import {Fuel, Category, Listing} from '@prisma/client';
-import { MakeOptional } from '@/types';
+import { SearchListing } from '@/types';
 
 export async function getListingById(id: number) {
 	return prisma.listing.findUnique({
@@ -10,11 +10,25 @@ export async function getListingById(id: number) {
 	});
 }
 
-export async function getListings(listing: MakeOptional<Listing>) {
-	return prisma.listing.findMany({
-		where: {
-		}
-	});
+export async function getListings(listing: SearchListing) {
+	let where: any = {};
+	if (listing.sellerId) where.sellerId = listing.sellerId
+	if (listing.brandId) where.brandId = listing.brandId
+	if (listing.modelId) where.modelId = listing.modelId
+	if (listing.acceptsTrade) where.acceptsTrade = listing.acceptsTrade
+	if (listing.fuel) where.fuel = listing.fuel as Fuel
+	if (listing.category) where.category = listing.category as Category
+	if (listing.plateEnding) where.plateEnding = listing.plateEnding
+	if (listing.maxYear) where.year = { lte: listing.maxYear }
+	if (listing.minYear) where.year = { gte: listing.minYear }
+	if (listing.maxPrice) where.price = { lte: listing.maxPrice }
+	if (listing.minPrice) where.price = { gte: listing.minPrice }
+	if (listing.maxMileage) where.mileage = { lte: listing.maxMileage }
+	if (listing.minMileage) where.mileage = { gte: listing.minMileage }
+	if (listing.state) where.state = listing.state
+	if (listing.city) where.city = listing.city
+
+	return prisma.listing.findMany({ where });
 }
 
 export async function getBrands() {
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,21 @@
-import {Listing} from '@prisma/client';
-
 export type MakeOptional<T> = {
 	[K in keyof T]?: T[K];
 };
+
+export type SearchListing = {
+	sellerId?: number
+	brandId?: number
+	modelId?: number
+	acceptsTrade?: boolean
+	fuel?: string
+	category?: string
+	plateEnding?: number
+	maxYear?: number
+	minYear?: number
+	maxPrice?: number
+	minPrice?: number
+	maxMileage?: number
+	minMileage?: number
+	state?: string
+	city?: string
+}
